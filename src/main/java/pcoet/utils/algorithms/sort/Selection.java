09a16163bba208d6bd26c43014c86f1fe600d006
David Huntsperger <5672572+pcoet@users.noreply.github.com>
2019-07-04 13:11:44 -0700

    added insertion sort

--- src/main/java/pcoet/utils/algorithms/sort/Selection.java
+++ src/main/java/pcoet/utils/algorithms/sort/Selection.java
@@ -1,6 +1,6 @@
 package pcoet.utils.algorithms.sort;
 
-public class Selection {
+public class Selection extends Sort {
   /**
    * Sorts an array into increasing order. Runs in O(n2) time.
    * For more information, see https://en.wikipedia.org/wiki/Selection_sort
@@ -19,13 +19,5 @@ public class Selection {
     }
   }
 
-  private static <T extends Comparable<T>> boolean less(T v, T w) {
-    return v.compareTo(w) < 0;
-  }
 
-  private static <T extends Comparable<T>> void exchange(T[] arr, int i, int j) {
-    T t = arr[i];
-    arr[i] = arr[j];
-    arr[j] = t;
-  }
 }
--- src/test/java/pcoet/utils/algorithms/sort/InsertionTest.java
+++ src/test/java/pcoet/utils/algorithms/sort/InsertionTest.java
@@ -3,12 +3,12 @@ package pcoet.utils.algorithms.sort;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class SelectionTest {
+public class InsertionTest {
   @Test
   public void sortsIntegerArray() {
     Integer[] input = {12, 9, 3, 7, 14, 11, 6, 2, 10, 5};
     Integer[] expected = {2, 3, 5, 6, 7, 9, 10, 11, 12, 14};
-    Selection.sort(input);
+    Insertion.sort(input);
     assertArrayEquals(expected, input);
   }
 
@@ -31,7 +31,7 @@ public class SelectionTest {
         "Sam Eagle"
     };
 
-    Selection.sort(input);
+    Insertion.sort(input);
     assertArrayEquals(expected, input);
   }
 }
