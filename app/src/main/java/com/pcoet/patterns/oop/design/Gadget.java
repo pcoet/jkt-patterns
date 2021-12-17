cd1bb7641c1f47718fe47fd6338fd7e9c7a8a389
David Huntsperger <5672572+pcoet@users.noreply.github.com>
2021-12-17 09:01:03 -0800

    Added a static factory method to Gadget (#26)
    * added a static factory method to Gadget
    
    * correcting comment on method

--- app/src/main/java/com/pcoet/patterns/oop/design/Gadget.java
+++ app/src/main/java/com/pcoet/patterns/oop/design/Gadget.java
@@ -8,16 +8,26 @@ public class Gadget extends Widget {
   /**
    * Gadget constructor
    *
-   * @param name        The name of the gadget
-   * @param upperLeftX  The x value of the upper-left corner of the Widget
-   * @param upperLeftY  The y value of the upper-left corner of the Widget
-   * @param width       The width of the widget
-   * @param height      The height of the widget
+   * @param name        The name of the Gadget
+   * @param upperLeftX  The x value of the upper-left corner of the Gadget
+   * @param upperLeftY  The y value of the upper-left corner of the Gadget
+   * @param width       The width of the Gadget
+   * @param height      The height of the Gadget
    * @param buttonLabel A descriptive label for the button on the Gadget
    */
   Gadget(String name, int upperLeftX, int upperLeftY, int width, int height, String buttonLabel) {
     super(name, upperLeftX, upperLeftY, width, height);
     this.buttonLabel = buttonLabel;
+  }
+
+  /**
+   * Creates a 20 x 20 Gadget centered at (0,0).
+   * @param name The name of the Gadget
+   * @param buttonLabel A descriptive label for the button on the Gadget
+   * @return An instance of Gadget
+   */
+  public static Gadget createAtOrigin(String name, String buttonLabel) {
+    return new Gadget(name, -10, 10, 20, 20, buttonLabel);
   }
 
   public String getButtonLabel() {
