a7ab8fc03f7fd807f22d366be8cab4798ba68284
David Huntsperger <5672572+pcoet@users.noreply.github.com>
2022-02-21 11:52:21 -0800

    refactored gradle build to use both app and lib (#34)

---
+++ app/src/main/kotlin/jkt/patterns/app/App.kt
@@ -0,0 +1,7 @@
+package jkt.patterns.app
+
+import jkt.patterns.functional.add
+
+fun main() {
+    println(add(2, 2))
+}
---
+++ patterns/src/test/java/com/pcoet/patterns/data/structures/QueueTest.java
@@ -0,0 +1,48 @@
+package com.pcoet.patterns.data.structures;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueueTest {
+  Queue<String> queue;
+
+  private void setUpStack() {
+    queue = new Queue<>();
+    queue.enqueue("lions");
+    queue.enqueue("tigers");
+    queue.enqueue("bears");
+  }
+
+  public QueueTest() {
+    this.setUpStack();
+  }
+
+  @Test
+  public void queueWithItemsIsNotEmpty() {
+    assertEquals(false, queue.isEmpty());
+  }
+
+  @Test
+  public void queueHasExpectedSize() {
+    assertEquals(3, queue.size());
+  }
+
+  @Test
+  public void queueCanDequeueItems() {
+    assertEquals("lions", queue.dequeue());
+    assertEquals("tigers", queue.dequeue());
+    assertEquals("bears", queue.dequeue());
+    assertEquals(true, queue.isEmpty());
+  }
+
+  @Test
+  public void queueCanEnqueueAndDequeueInts() {
+    Queue<Integer> intQueue = new Queue<>();
+    intQueue.enqueue(1);
+    intQueue.enqueue(2);
+    intQueue.enqueue(3);
+    assertEquals(1, intQueue.dequeue());
+    assertEquals(2, intQueue.dequeue());
+    assertEquals(3, intQueue.dequeue());
+  }
+}
